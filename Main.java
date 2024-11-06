69cad6b194c7f95d8232581b8d72830dba2cfbcd
HP <ksaud3508@gmail.com>
2024-11-06 18:32:19 +0500

    Documentation

--- Main.java
+++ Main.java
@@ -7,8 +7,6 @@ public class Main {
     public static void main(String[] args) {
         AudioUploaderView loginView = new AudioUploaderView();
         UploadView uploadView = new UploadView();
-
-      
         AdminController adminController = new AdminController(loginView, uploadView);
         UploadController uploadController = new UploadController(uploadView);
 
